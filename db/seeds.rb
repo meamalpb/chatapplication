2ac2facc2f912a0ec1d1c444cd14818cbb7dc397
meamalpb <321amalpb.com@gmail.com>
2023-08-15 09:05:07 +0530

    devise added and also cleaned some views

--- db/seeds.rb
+++ db/seeds.rb
@@ -4,4 +4,12 @@
 # Examples:
 #
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
-#   Character.create(name: "Luke", movie: movies.first)
+#   Character.create(name: "Luke", movie: movies.first) 
+
+20.times do
+User.create([{
+    username: Faker::Name.unique.name,
+    password: "123123",
+    email: Faker::Internet.email
+}])
+end
